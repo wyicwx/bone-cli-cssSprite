1aed2ecb9d73f454b04515eb215d11a3d7b3a3e5
wyicwx <wyicwx@gmail.com>
2015-05-02 09:15:39 +0800

    update

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var cssSprite = require('css-sprite');
 
 module.exports = function(command, bone) {
 	var cmder = command('css-sprite')
-		.option('--src [source]', 'glob strings to find source images to put into the sprite')
-		.option('--out [filepath]', 'path of directory to write sprite file to')
+		.option('--src <source>', 'glob strings to find source images to put into the sprite')
+		.option('--out <filepath>', 'path of directory to write sprite file to')
 	 	.option('-b, --base64        ', 'create css with base64 encoded sprite (css file will be written to <out>)')
 		.option('-c, --css-image-path [cssImagePath]', 'http path to images on the web server (relative to css path or absolute path)  [../images]')
 		.option('-f, --format [format]', 'output format of the sprite (png or jpg)  [png]')
@@ -11,7 +11,7 @@ module.exports = function(command, bone) {
 		.option('-p, --processor [processor]', 'output format of the css. one of css, less, sass, scss or stylus  [css]')
 		.option('-t, --template [template]', 'output template file, overrides processor option')
 		.option('-r, --retina        ', 'generate both retina and standard sprites. src images have to be in retina resolution (doubled dimensions, with even-numbered height and width)')
-		.option('-s, --style [style]', 'file to write css to, if omitted no css is written')
+		.option('-s, --style <style>', 'file to write css to, if omitted no css is written')
 		.option('--background [background]', 'background color of the sprite in hex  [#FFFFFF]')
 		.option('--margin [margin]', 'margin in px between tiles  [4]')
 		.option('--opacity [opacity]', 'background opacity of the sprite. defaults to 0 when png or 100 when jpg  [0]')
